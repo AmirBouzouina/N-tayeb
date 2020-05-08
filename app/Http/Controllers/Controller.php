be5de5a25384808ec59aa5eeb371aaa2ba3a10df
Amir <amir.bouzouina@growdz.com>
2020-05-08 23:35:21 +0100

    Revert "testing"
    This reverts commit c8ff6777909ded0e9b52d995479f9b4e328e22f7.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use Intervention\Image\ImageManagerStatic as Image;
+
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
